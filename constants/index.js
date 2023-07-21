ab105307d423ff5d852df8046172f9f09bc80b5d
vahidhabibi <62717087+vahidhabibi@users.noreply.github.com>
2023-07-21 13:08:57 +0330

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xe26ecf32448bfb7088643480444bab8e1338d757";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
